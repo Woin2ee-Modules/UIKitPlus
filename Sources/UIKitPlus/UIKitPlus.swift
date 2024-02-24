ceca86b10c2ccdf0e1d5c2e8783b60dbc6e742b9
Jaewon Yun <woin2ee@gmail.com>
2024-02-24 22:55:03 +0900

    Update documentation comments

--- Sources/UIKitPlus/UIKitPlus.swift
+++ Sources/UIKitPlus/UIKitPlus.swift
@@ -1,9 +1,9 @@
 import Foundation
 
-/// <#Description#>
+/// A protocol that provides a type property `reuseIdentifier`.
 public protocol ReuseIdentifying {
 
-    /// The `reuseIdentifier`. If you didn't override this property, value is name of type.
+    /// The `reuseIdentifier`. If you didn't override this property, value is name of concrete type.
     static var reuseIdentifier: String { get }
 }
 
